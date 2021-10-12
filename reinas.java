1c308505abdd15c43b36b30f4736f24e8e44b181
sessionmaestra <70710866+sessionmaestra@users.noreply.github.com>
2021-10-12 00:18:34 -0300

    funcion imprimirMatriz

--- reinas.java
+++ reinas.java
@@ -4,37 +4,23 @@ public class reinas{
     public static void main(String[] args) {
         int tamanoTablero = 4;
         int poblacion = 4;
-
-        Random r = new Random();
-
         int[][] tableros = new int[poblacion][tamanoTablero];
+        Random r = new Random();
 
         //inicializacion tableros
         for(int i=0; i<poblacion; i++){
-        
             for(int j=0; j<tamanoTablero; j++){
-            
                 tableros[i][j]=j+1;
             }
-  
         }
 
         //muestra tableros inicializados
-        for (int x=0; x < tableros.length; x++) {
-            for (int y=0; y < tableros[x].length; y++) {
-              System.out.print (tableros[x][y]);
-              if (y!=tableros[x].length-1) System.out.print("\t");
-            }
-            System.out.println("\n");
-          }
-          System.out.println("\n\n\n");
+        imprimirMatriz(tableros);
 
 
         //desordena los tableros
         for(int i=0; i<poblacion; i++){
-        
             for(int j=0; j<tamanoTablero; j++){
-            
                 int posAleatoria = r.nextInt(tamanoTablero);
                 int temp = tableros[i][j];
                 tableros[i][j] = tableros[i][posAleatoria];
@@ -44,13 +30,7 @@ public class reinas{
         }
 
         //muestra tableros desordenados
-        for (int x=0; x < tableros.length; x++) {
-            for (int y=0; y < tableros[x].length; y++) {
-              System.out.print (tableros[x][y]);
-              if (y!=tableros[x].length-1) System.out.print("\t");
-            }
-            System.out.println("\n");
-          }
+        imprimirMatriz(tableros);
            
         
 
@@ -64,39 +44,38 @@ public class reinas{
             for(int j=0; j<tamanoTablero; j++){
               auxdiagonal[j] = ((j+1) - tableros[i][j]);
               auxinversa[j] = Math.abs((j+1) +tableros[i][j]);  
-              //System.out.print(auxdiagonal[j]+"\t");
-              //System.out.println(auxinversa[j]);
-
             }
             
 
             for(int z=0; z<tamanoTablero; z++){
                 for(int x=0; x<tamanoTablero; x++){
-                    
                     if(z!=x){
                         if(auxdiagonal[z]==auxdiagonal[x]){
-
                             fitness=fitness+1;
-                            //System.out.println("aca sume 1 en diagonal:"+auxdiagonal[z]+" "+auxdiagonal[x]);
                         }    
                         if(auxinversa[z]==auxinversa[x]){
-
                             fitness=fitness+1;
-                            //System.out.println("aca sume 1 en inversa:"+auxdiagonal[z]+" "+auxdiagonal[x]);
                         } 
                     }
-
-                      
                 }
                 
             }
             System.out.println("fitness tablero " +(i+1)+": "+fitness/2 +"\n");
-  
         }
-       
-        
-        
-        
-
     }
+
+    /* 
+    Funcion para imprimir las matrices por pantalla
+    */
+    public static void imprimirMatriz( int[][] matriz){
+        for (int x=0; x < matriz.length; x++) {
+            for (int y=0; y < matriz[x].length; y++) {
+              System.out.print (matriz[x][y]);
+              if (y!=matriz[x].length-1) System.out.print("\t");
+            }
+            System.out.println("\n");
+          }    
+    }
+
+
 }
